5611df300977ee027e431a9eec7ae6144f2534ef
DennerDuarte <rm551938@fiap.com.br>
2023-05-19 12:16:27 -0300

    validação por token e btn para sair

--- js/login.js
+++ js/login.js
@@ -1,56 +1,64 @@
-// //criando lsita de objetos
+//CRIANDO LISTA DE OBJETOS
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "Denden da Silva",
+        nomeUsuario : "denden",
+        senhaUsuario : "123456"
+    },
+    { 
+        nomeCompleto : "Gersu da Silva",
+        nomeUsuario : "gege",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "José da Silva",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Paulo das Couves",
+        nomeUsuario : "paulo",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Mary Help",
+        nomeUsuario : "mary",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Pedro Silva",
+        nomeUsuario : "pedro",
+        senhaUsuario : "123456"
+    }
+];
 
-// let listaDeUsuarios = [
-//     {
-//         nomeCompleto : "Denden da Silva",
-//         nomeUsuario : "denden",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Gersu da Silva",
-//         nomeUsuario : "gege",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "José da Silva",
-//         nomeUsuario : "jose",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Paulo das Couves",
-//         nomeUsuario : "paulo",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Mary Help",
-//         nomeUsuario : "mary",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Pedro Silva",
-//         nomeUsuario : "pedro",
-//         senhaUsuario : "123456"
-//     },
-// ];
-
-// //Guardar a lista de objetos no local-storage
-// localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
-
+//GUARDAR A LISTA DE OBJETOS NO LOCAL-STORAGE
+localStorage.setItem( "listaUser",  JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
     const inputUser = document.querySelector("#idUser");
     const inputPass = document.querySelector("#idPass");
 
+    //CRIANDO O OBJETO USER-LOGADO
+    const usuarioLogado = {
+        nomeUsuarioLogado : inputUser.value,
+        senhaUsuarioLogado: inputPass.value
+    }
+
+    //CRIANDO O OBJETO USUÁRIO-VALIDADO
+    let usuarioValidado = {};
+
     if(evt.target.id == "btnSubmit"){
         
         try {
-
-            //recuperar a lista de usuarios do local storage
+          
+            //Recuperar a lista de usuários do LocalStorage
             let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
-            
+
             listaDeUsuarios.forEach((usuario)=>{
                 
-                if(inputUser.value == usuario.nomeUsuario && inputPass.value == usuario.senhaUsuario){
+                if(usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario){
+                    usuarioValidado = usuario;
                     throw "VALIDADO";
                 }
             });
@@ -61,16 +69,25 @@ addEventListener("click", (evt)=>{
             const msgError = document.querySelector("#msgError");
             if(msg == "VALIDADO"){
                 msgError.setAttribute("style","color:#00ff00;");
-                msgError.innerHTML = "<span><strong>Login efetuado com Sucesso!</strong></span>";
-                setTimeout( function (){
-                    window.location.href = "../pages/sucesso.html";
+                msgError.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} efetuou o login com sucesso!</strong></span>`;
                 
+                //adicionado o objeto usuario-validado
+                localStorage.setItem("user-validado", JSON.stringify(usuarioValidado));
+
+                //Criando token de autenticação
+                let token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+                //adicionando o token no localStorage
+                localStorage.setItem("user-token", token);
+
+
+                //Redirect : Espera 3 segundos antes de redirecionar
+                setTimeout( function(){
+                    window.location.href = "../pages/sucesso.html";
                 }, 3000);
+
             }else{
                 msgError.setAttribute("style","color:#ff0000;");
                 msgError.innerHTML = "<span><strong>Usuário ou senha inválidos!</strong></span>";
-                //redirect espera 3 segundos antes de redirecionar
-                
             }
         }
 
@@ -84,8 +101,8 @@ addEventListener("click", (evt)=>{
             inputPass.setAttribute("type","text");
         }else{
         //Ocultando a senha!
+        evt.target.setAttribute("class","fa fa-eye");
             inputPass.setAttribute("type","password");
-            evt.target.setAttribute("class","fa fa-eye");
         }
         
     }
